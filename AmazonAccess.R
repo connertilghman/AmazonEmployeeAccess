7f9d97d5a3da2e4ca364ed4f6f40d224af0f9992
connertilghman <conner.tilghman@gmail.com>
2025-10-08 11:11:46 -0600

    changed nominal to numeric in my recipe

--- AmazonAccess.R
+++ AmazonAccess.R
@@ -19,9 +19,10 @@ ggplot(train, aes(factor(ACTION))) +
 create_report(train)
 
 my_recipe <- recipe(ACTION ~ ., data = train) |>
-  step_mutate_at(all_nominal_predictors(), fn = factor) |>
+  step_mutate_at(all_numeric_predictors(), fn = factor) |>
   step_other(all_nominal_predictors(), threshold = 0.001) |>
   step_dummy(all_nominal_predictors())
 
 prep_rec <- prep(my_recipe)
 baked_data <- bake(prep_rec, new_data = train)
+cat(ncol(baked_data))
