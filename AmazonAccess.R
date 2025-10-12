1ceae897b7baa6d00da12e6727d2da7e84d43892
connertilghman <conner.tilghman@gmail.com>
2025-10-12 15:12:07 -0600

    penalized regression added

--- AmazonAccess.R
+++ AmazonAccess.R
@@ -14,12 +14,12 @@ train <- train |>
   mutate(ACTION = as.factor(ACTION))
 
 
-ggplot(train, aes(factor(ACTION))) +
-  geom_bar(fill = "steelblue") +
-  labs(title = "Access Decision Distribution", x = "ACTION", y = "Count")
-
-
-create_report(train)
+# ggplot(train, aes(factor(ACTION))) +
+#   geom_bar(fill = "steelblue") +
+#   labs(title = "Access Decision Distribution", x = "ACTION", y = "Count")
+# 
+# 
+# create_report(train)
 
 my_recipe <- recipe(ACTION ~ ., data = train) |>
   step_mutate_at(all_numeric_predictors(), fn = factor) |>
@@ -30,18 +30,45 @@ prep_rec <- prep(my_recipe)
 baked_data <- bake(prep_rec, new_data = train)
 cat(ncol(baked_data))
 
-logRegModel <- logistic_reg() |>
-  set_engine("glm")
+# logRegModel <- logistic_reg() |>
+#   set_engine("glm")
+# 
+# logReg_workflow <- workflow() |>
+#   add_model(logRegModel) |>
+#   add_recipe(my_recipe)
+# 
+# logReg_fit <- fit(logReg_workflow, data = train)
+# 
+# amazon_predictions <- predict(logReg_fit,
+#                               new_data=test,
+#                               type="prob")
 
-logReg_workflow <- workflow() |>
-  add_model(logRegModel) |>
-  add_recipe(my_recipe)
+my_mod <- logistic_reg(mixture=tune(), penalty=tune()) |>
+  set_engine("glmnet")
 
-logReg_fit <- fit(logReg_workflow, data = train)
+amazon_workflow <- workflow() |>
+  add_recipe(my_recipe) |>
+  add_model(my_mod)
 
-amazon_predictions <- predict(logReg_fit,
-                              new_data=test,
-                              type="prob")
+tuning_grid <- grid_regular(penalty(),
+                            mixture(),
+                            levels = 5) 
+folds <- vfold_cv(train, v = 10, repeats=1)
+
+CV_results <- amazon_workflow |>
+  tune_grid(resamples=folds,
+          grid=tuning_grid,
+          metrics=metric_set(roc_auc))
+
+bestTune <- CV_results |>
+  select_best(metric = "roc_auc")
+
+final_wf <- amazon_workflow |>
+  finalize_workflow(bestTune) |>
+  fit(data=train)
+
+penalized_preds <- predict(final_wf, new_data = test, type="prob")
+
 kaggle_submission <- test|>
   bind_cols(amazon_predictions)|>
   select(id, .pred_1) |>
